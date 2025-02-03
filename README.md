fb24ea594f21eeb34354b44458b01b9b73c125e4
Zahra'a Alderawi <derawi.2000@gmail.com>
2025-02-03 21:08:04 +0200

    add DuckDuckGo

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # RealTimeSearch
 
+### Make sure duckduckgo is installed:
+
+```bash
+pip install duckduckgo-search
+```
+
 ### Make sure Streamlit is installed:
 ```bash
 pip install streamlit
@@ -9,8 +15,10 @@ pip install streamlit
 ### Running the Streamlit Application
 
 To run the application, use the following command in your terminal or command prompt:
+Replace the current path with your `app.py` file path
 ```bash
- streamlit run c:/Users/H.P/Desktop/RealTimeSearch/app.py
+streamlit run D:/GitHub/RealTimeSearch/app.py
+
 ```
 
 
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-from search_system import combine_search_results
 import json
+from search_system import combine_search_results  
 
 def load_api_keys(config_file='config.json'):
     with open(config_file, 'r') as f:
@@ -15,20 +15,32 @@ cx = keys.get('CX')
 st.title("Search Results Viewer")
 query = st.text_input("Enter your search query:", key="search_query_input")
 
-trusted_domains = ['.edu', '.gov', '.net', '.org', '.com', 'trustedwebsite.com']
+trusted_domains = ['.edu', '.gov', '.net', '.org', '.com']
+
 if query and api_key and cx:
     results = combine_search_results(query, api_key, cx, trusted_domains)
     if results:
-        # Display each result as before
-        for idx, result in enumerate(results, 1):
-            st.subheader(f" {result['title']}")
-            st.write(f" [{result['url']}]({result['url']})")
+        # Display Google results first
+        st.markdown("# Google Search Results")
+        google_results = results[:5] 
+        for idx, result in enumerate(google_results, 1):
+            st.subheader(f"{result['title']}")
+            st.write(f"[{result['url']}]({result['url']})")
             st.write(f"{result['snippet']}")
             st.markdown("---")
         
+        # Display DuckDuckGo results
+        st.markdown("# DuckDuckGo Search Results")
+        duckduckgo_results = results[5:10]  
+        for idx, result in enumerate(duckduckgo_results, 1):
+            st.subheader(f"{result['title']}")
+            st.write(f"[{result['url']}]({result['url']})")
+            st.write(f"{result['snippet']}")
+            st.markdown("---")
+
         df = pd.DataFrame(results, columns=['title', 'url', 'snippet'])
         st.markdown("### Results DataFrame")
-        st.dataframe(df)  
-        
+        st.dataframe(df)
+
 else:
     st.write("Please enter a search query.")
